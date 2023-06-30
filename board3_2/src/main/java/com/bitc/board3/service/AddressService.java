80831695949eeab40fec29688c4e7f9ada6329d1
Wookicook2 <sso1604@naver.com>
2023-06-30 17:04:51 +0900

    board3에서 boardlist.html만 조금 변경함 추후 합칠것

---
+++ board3_2/src/main/java/com/bitc/board3/service/AddressService.java
@@ -0,0 +1,11 @@
+package com.bitc.board3.service;
+
+import com.bitc.board3.dto.AddressDto;
+import com.github.pagehelper.Page;
+
+public interface AddressService {
+
+  Page<AddressDto> getAddressList(int pageNum) throws Exception;
+
+
+}
